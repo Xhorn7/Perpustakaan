b92df268c0332b55d8661391021129a4aed4381a
Xhorn7 <samuel.alfonsus@binus.ac.id>
2024-11-24 19:58:36 +0700

    adding faUserSecret, faFolderPlus, faEye, faTrash, faSave, faSignOutAlt

--- perpus-frontend/src/main.js
+++ perpus-frontend/src/main.js
@@ -6,6 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { faUserSecret, faFolderPlus, faEye, faTrash, faSave, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+
+library.add(faUserSecret, faFolderPlus, faEye, faTrash, faSave, faSignOutAlt)
+
 const app = createApp(App)
 
 app.use(createPinia())
