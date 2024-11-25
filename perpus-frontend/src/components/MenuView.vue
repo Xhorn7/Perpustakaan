18e56c14287b38fc738366125d1d618c4ee986b6
Xhorn7 <samuel.alfonsus@binus.ac.id>
2024-11-25 11:25:57 +0700

    new ui ux

--- perpus-frontend/src/components/MenuView.vue
+++ perpus-frontend/src/components/MenuView.vue
@@ -2,7 +2,6 @@
 import axios from "axios";
 import { useRouter, useRoute } from "vue-router";
 import { useUserStore } from "../stores/counter";
-import { storeToRefs, createPinia } from "pinia";
 import { ref, onMounted, onUnmounted, computed } from "vue";
 import {
     XIcon,
@@ -18,17 +17,15 @@ import {
     ChevronDownIcon,
 } from "lucide-vue-next";
 
-const pini = createPinia();
 const store = useUserStore();
 const therouter = useRouter();
-const route = useRoute();
+const theroute = useRoute();
 const customConfig = {
     Authorization: "Bearer " + store.token,
 };
 
 const isOpen = ref(window.innerWidth >= 1024);
 const isLibraryOpen = ref(false);
-const theroute = useRoute();
 
 const toggleSidebar = () => {
     isOpen.value = !isOpen.value;
@@ -54,17 +51,15 @@ const ismenu = computed(() => {
     return theroute.name == "menu" ? true : false;
 });
 
-const currentRoute = computed(() => route.name);
+const currentRoute = computed(() => theroute.name);
 
-function logout() {
-    console.log(customConfig);
+const logout = () => {
     axios({
         url: "http://localhost:8000/api/logout",
         method: "get",
         headers: customConfig,
     })
         .then((response) => {
-            console.log(response.data);
             if (response.data.success === true) {
                 store.reset();
                 therouter.push("/");
@@ -72,13 +67,12 @@ function logout() {
         })
         .catch((error) => {
             console.log("AJAX" + error);
-        })
-        .finally();
-}
+        });
+};
 </script>
 
 <template>
-    <div class="flex items-center justify-center gap-32">
+    <div class="flex">
         <button
             @click="toggleSidebar"
             class="fixed z-30 p-2 rounded-md shadow-md left-4 top-5 focus:outline-none focus:ring-2 focus:ring-gray-200 lg:hidden"
@@ -115,7 +109,7 @@ function logout() {
                 <nav class="flex-1 p-4 overflow-y-auto">
                     <ul class="space-y-2">
                         <li>
-                            <RouterLink
+                            <router-link
                                 to="/pegawai"
                                 class="flex items-center p-2 rounded-md hover:bg-gray-800"
                                 :class="{
@@ -123,11 +117,11 @@ function logout() {
                                 }"
                             >
                                 <UsersIcon class="w-5 h-5 mr-3" />
-                                PEGAWAI
-                            </RouterLink>
+                                Pegawai
+                            </router-link>
                         </li>
                         <li>
-                            <RouterLink
+                            <router-link
                                 to="/barang"
                                 class="flex items-center p-2 rounded-md hover:bg-gray-800"
                                 :class="{
@@ -135,8 +129,8 @@ function logout() {
                                 }"
                             >
                                 <PackageIcon class="w-5 h-5 mr-3" />
-                                BARANG
-                            </RouterLink>
+                                Barang
+                            </router-link>
                         </li>
 
                         <li>
@@ -146,7 +140,7 @@ function logout() {
                             >
                                 <div class="flex items-center">
                                     <LibraryIcon class="w-5 h-5 mr-3" />
-                                    PERPUSTAKAAN
+                                    Perpustakaan
                                 </div>
                                 <ChevronDownIcon
                                     class="w-4 h-4 transition-transform"
@@ -159,7 +153,7 @@ function logout() {
                                 class="px-2 mt-2 space-y-1"
                             >
                                 <li>
-                                    <RouterLink
+                                    <router-link
                                         to="/buku"
                                         class="flex items-center p-2 pl-10 rounded-md hover:bg-gray-800"
                                         :class="{
@@ -169,40 +163,47 @@ function logout() {
                                     >
                                         <BookIcon class="w-4 h-4 mr-3" />
                                         Buku
-                                    </RouterLink>
+                                    </router-link>
                                 </li>
                                 <li>
-                                    <RouterLink
+                                    <router-link
                                         to="/peminjaman"
                                         class="flex items-center p-2 pl-10 rounded-md hover:bg-gray-800"
+                                        :class="{
+                                            'bg-gray-800':
+                                                currentRoute === 'peminjaman',
+                                        }"
                                     >
                                         <BookOpenIcon class="w-4 h-4 mr-3" />
                                         Peminjaman
-                                    </RouterLink>
+                                    </router-link>
                                 </li>
                                 <li>
-                                    <RouterLink
+                                    <router-link
                                         to="/pembelian"
                                         class="flex items-center p-2 pl-10 rounded-md hover:bg-gray-800"
+                                        :class="{
+                                            'bg-gray-800':
+                                                currentRoute === 'pembelian',
+                                        }"
                                     >
                                         <ShoppingCartIcon
                                             class="w-4 h-4 mr-3"
                                         />
                                         Pembelian
-                                    </RouterLink>
+                                    </router-link>
                                 </li>
                             </ul>
                         </li>
 
                         <li class="mt-auto">
-                            <a
-                                href="#"
-                                class="flex items-center p-2 text-red-400 rounded-md hover:bg-gray-800"
+                            <button
                                 @click="logout"
+                                class="flex items-center p-2 text-red-400 rounded-md hover:bg-gray-800"
                             >
                                 <LogOutIcon class="w-5 h-5 mr-3" />
-                                LOGOUT
-                            </a>
+                                Logout
+                            </button>
                         </li>
                     </ul>
                 </nav>
