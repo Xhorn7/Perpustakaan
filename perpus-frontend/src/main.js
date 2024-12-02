5ce4f90a5f70ca78ea51bae77f80f258730b7fcf
Xhorn7 <samuel.alfonsus@binus.ac.id>
2024-12-02 21:37:18 +0700

    import VueAwesomePaginate to main.js

--- perpus-frontend/src/main.js
+++ perpus-frontend/src/main.js
@@ -10,6 +10,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faFolderPlus, faEye, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+import { VueAwesomePaginate } from 'vue-awesome-paginate';
+import "../node_modules/vue-awesome-paginate/dist/style.css";
+
 library.add(faFolderPlus, faEye, faTrash)
 
 const app = createApp(App)
@@ -18,5 +21,7 @@ app.use(createPinia())
 app.use(router)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.component(VueAwesomePaginate);
 
 app.mount('#app')
